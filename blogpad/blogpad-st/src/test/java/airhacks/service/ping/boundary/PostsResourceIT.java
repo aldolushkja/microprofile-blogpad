b3d14c7ce82c4d58ffef45551e6c228586652974
alushkja <aldo.lushkja@consulenti.fastweb.it>
2020-07-07 23:07:06 +0200

    system testing the put

--- blogpad/blogpad-st/src/test/java/airhacks/service/ping/boundary/PostsResourceIT.java
+++ blogpad/blogpad-st/src/test/java/airhacks/service/ping/boundary/PostsResourceIT.java
@@ -4,19 +4,20 @@ import org.eclipse.microprofile.rest.client.RestClientBuilder;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import javax.json.Json;
+import javax.json.JsonObject;
 import javax.ws.rs.core.Response;
 import java.net.URI;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 /**
  *
  * @author airhacks.com
  */
-public class PingResourceIT {
+public class PostsResourceIT {
 
-    private PingResourceClient client;
+    private PostsResourceClient client;
 
     @BeforeEach
     public void init() {
@@ -24,18 +25,19 @@ public class PingResourceIT {
         this.client = RestClientBuilder.
                 newBuilder().
                 baseUri(uri).
-                build(PingResourceClient.class);
+                build(PostsResourceClient.class);
 
     }
 
     @Test
-    public void ping() {
-        Response response = this.client.ping();
+    public void save() {
+        JsonObject post = Json.createObjectBuilder()
+                .add("title", "remote_hello")
+                .add("content", "first st")
+                .build();
+        Response response = this.client.save(post);
         int status = response.getStatus();
-        assertEquals(200, status);
-        String message = response.readEntity(String.class);
-        assertNotNull(message);
-        System.out.println(message);
+        assertEquals(204, status);
         
     }
 }
