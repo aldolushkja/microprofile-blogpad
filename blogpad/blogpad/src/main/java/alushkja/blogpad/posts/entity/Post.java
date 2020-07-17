b2a9c3e05530974daf70e7db02a7f46dbcf23f4f
alushkja <aldo.lushkja@consulenti.fastweb.it>
2020-07-17 23:20:14 +0200

    readonly and required schema

--- blogpad/blogpad/src/main/java/alushkja/blogpad/posts/entity/Post.java
+++ blogpad/blogpad/src/main/java/alushkja/blogpad/posts/entity/Post.java
@@ -1,19 +1,27 @@
 package alushkja.blogpad.posts.entity;
 
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+
 import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 
 public class Post {
 
+    @Schema(readOnly = true)
     public String fileName;
 
+    @Schema(required = true)
     @Size(min = 3, max = 255)
     public String title;
 
+    @Schema(required = true)
     @Size(min = 3)
     public String content;
 
+    @Schema(readOnly = true)
     public LocalDateTime createdAt;
+
+    @Schema(readOnly = true)
     private LocalDateTime modifiedAt;
 
     public Post(String title, String content) {
