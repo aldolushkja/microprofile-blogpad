2345048a4fd2dcc10e5142ae0bb3075685450770
alushkja <aldo.lushkja@consulenti.fastweb.it>
2020-07-25 15:23:35 +0200

    open tracing with jaeger

--- blogpad/content/content/src/main/java/alushkja/blogpad/posts/boundary/PostsResource.java
+++ blogpad/content/content/src/main/java/alushkja/blogpad/posts/boundary/PostsResource.java
@@ -5,6 +5,7 @@ import alushkja.blogpad.posts.entity.Post;
 import org.eclipse.microprofile.metrics.annotation.Counted;
 import org.eclipse.microprofile.metrics.annotation.Timed;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.opentracing.Traced;
 
 import javax.inject.Inject;
 import javax.validation.Valid;
@@ -42,6 +43,7 @@ public class PostsResource {
 
     @Timed
     @GET
+    @Traced
     @Path("{title}")
     @Produces(MediaType.APPLICATION_JSON)
     public Post findPost(@PathParam("title") String title) {
