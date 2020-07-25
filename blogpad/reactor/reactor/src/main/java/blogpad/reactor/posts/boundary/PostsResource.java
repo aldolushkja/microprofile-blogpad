92abbe90dfbead93ab2b8079de5252f0a3beb181
alushkja <aldo.lushkja@consulenti.fastweb.it>
2020-07-25 14:36:16 +0200

    rest client and config

--- blogpad/reactor/reactor/src/main/java/blogpad/reactor/posts/boundary/PostsResource.java
+++ blogpad/reactor/reactor/src/main/java/blogpad/reactor/posts/boundary/PostsResource.java
@@ -1,5 +1,10 @@
 package blogpad.reactor.posts.boundary;
 
+import blogpad.reactor.posts.control.PostsResourceClient;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+
+import javax.inject.Inject;
+import javax.json.JsonObject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -9,11 +14,16 @@ import javax.ws.rs.core.MediaType;
 @Path("posts")
 public class PostsResource {
 
+    @Inject
+    @RestClient
+    PostsResourceClient client;
+
     @GET
     @Path("{title}")
     @Produces(MediaType.TEXT_HTML)
     public String findPost(@PathParam("title") String title) {
-        return "<h1>hello<h1>" + title;
+        var response = this.client.findPost("initial");
+        return "<h1>hello<h1>" + title + response.readEntity(JsonObject.class);
     }
 
     ;
