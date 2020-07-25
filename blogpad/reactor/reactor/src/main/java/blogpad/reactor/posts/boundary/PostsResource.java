739c6cbd13be90f2c801cf8d43b28909b23277b4
alushkja <aldo.lushkja@consulenti.fastweb.it>
2020-07-25 14:47:21 +0200

    adding comunication metrics

--- blogpad/reactor/reactor/src/main/java/blogpad/reactor/posts/boundary/PostsResource.java
+++ blogpad/reactor/reactor/src/main/java/blogpad/reactor/posts/boundary/PostsResource.java
@@ -1,10 +1,6 @@
 package blogpad.reactor.posts.boundary;
 
-import blogpad.reactor.posts.control.PostsResourceClient;
-import org.eclipse.microprofile.rest.client.inject.RestClient;
-
 import javax.inject.Inject;
-import javax.json.JsonObject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -15,17 +11,13 @@ import javax.ws.rs.core.MediaType;
 public class PostsResource {
 
     @Inject
-    @RestClient
-    PostsResourceClient client;
+    Reactor reactor;
 
     @GET
     @Path("{title}")
     @Produces(MediaType.TEXT_HTML)
     public String findPost(@PathParam("title") String title) {
-        var response = this.client.findPost("initial");
-        return "<h1>hello<h1>" + title + response.readEntity(JsonObject.class);
+        return this.reactor.render(title);
     }
-
-    ;
 
 }
